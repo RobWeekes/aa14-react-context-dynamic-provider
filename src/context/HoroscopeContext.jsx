b892fd5839734c551c5e08a621b40630fe0b91a2
RobWeekes <157428964+RobWeekes@users.noreply.github.com>
2025-02-10 20:05:41 -0800

    Create a provider component that holds your Context.Provider Wrap your entire App with that provider component Set state variables as your provider component's value prop so that you can change the value dynamically Consume the context in another component Update (manually) your provider's default value and have that change reflected in another component

--- src/context/HoroscopeContext.jsx
+++ src/context/HoroscopeContext.jsx
@@ -1,12 +1,16 @@
 import { createContext, useState } from 'react';
+import horoscopesObj from '../data/horoscopes';
 
 export const HoroscopeContext = createContext();
 
 export function HoroscopeProvider(props) {
-  const [currentSign, setCurrentSign] = useState({sign: 'Capricorn'});
-
+  const [currentSign, setCurrentSign] = useState('Sagittarius');
+  console.log('currentSign:', currentSign);
+  const sign = horoscopesObj[currentSign];
+  // console.log('sign from HoroscopeContext:', sign);
+  // 'sign' should be an object assigned to current astro sign from useState above
   return (
-    <HoroscopeContext.Provider value={{ currentSign, setCurrentSign }}>
+    <HoroscopeContext.Provider value={{ sign, setCurrentSign }}>
       {props.children}
     </HoroscopeContext.Provider>
   )

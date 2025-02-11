bc728126fa3f72475c60420ed77444082218840d
RobWeekes <157428964+RobWeekes@users.noreply.github.com>
2025-02-10 19:13:42 -0800

    added value prop to HoroscopeProvider

--- src/context/HoroscopeContext.jsx
+++ src/context/HoroscopeContext.jsx
@@ -1,3 +1,13 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 export const HoroscopeContext = createContext();
+
+export function HoroscopeProvider(props) {
+  const [currentSign, setCurrentSign] = useState({sign: 'Capricorn'});
+
+  return (
+    <HoroscopeContext.Provider value={{ currentSign, setCurrentSign }}>
+      {props.children}
+    </HoroscopeContext.Provider>
+  )
+}
